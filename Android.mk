ca5c8bdc146d30819b038d021b71985b5e7c9549
allen.you <allen.you@tinno.com>
2018-09-03 19:10:09 +0800

    <REQ><SCM-4><import baseline ALPS-RELEASE-P0.MP1-V1.0.1 20180903>
    Change-Id: I218d3dc59458f99e14e7bd93e2b2b57b7d8adfa6

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 ifneq (,$(filter $(word 2,$(subst -, ,$(LINUX_KERNEL_VERSION))),$(subst /, ,$(LOCAL_PATH))))
 
 MY_KERNEL_ROOT_DIR := $(PWD)
-MY_KERNEL_CONFIG_FILE := $(MY_KERNEL_ROOT_DIR)/kernel-4.4/arch/$(TARGET_ARCH)/configs/$(KERNEL_DEFCONFIG)
+MY_KERNEL_CONFIG_FILE := $(MY_KERNEL_ROOT_DIR)/kernel-4.4/arch/$(KERNEL_TARGET_ARCH)/configs/$(KERNEL_DEFCONFIG)
 MY_KERNEL_CONFIG_MODULES := $(shell grep ^CONFIG_MODULES=y $(MY_KERNEL_CONFIG_FILE))
 
 MY_KERNEL_DEFAULT_CONFIG_FILE := $(MY_KERNEL_ROOT_DIR)/kernel-4.4/drivers/misc/mediatek/Kconfig.default
@@ -21,13 +21,6 @@ ifeq ($(CONFIG_MODULES_EXIST), y)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := met.ko
-
-ifeq (user,$(TARGET_BUILD_VARIANT))
-   MET_INTERNAL_USE := $(shell test -f $(MY_KERNEL_ROOT_DIR)/vendor/mediatek/kernel_modules/met_drv_secure/4.4/init.met.rc && echo yes)
-   ifeq ($(MET_INTERNAL_USE),yes)
-      LOCAL_INIT_RC := ../../met_drv_secure/4.4/init.met.rc
-   endif
-endif
 
 include $(MTK_KERNEL_MODULE)
 else
