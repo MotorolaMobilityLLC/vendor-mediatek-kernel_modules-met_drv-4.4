31d2fdc21abbc3ddfe10bf84fbba2f016129b665
weichuan.lin <weichuan.lin@mediatek.com>
2019-01-22 19:18:31 +0800

    [ALPS03967548] MET: check CONFIG_MODULES exist
    [Detail]
    add check default kernel config path
    drivers/misc/mediatek/Kconfig.default
    
    MTK-Commit-Id: a7d7b341fdd5e62662d1d03e0f62f4de88d74069
    
    Change-Id: Ieb4ba9204a5e718bf8fa9c4e473fe35ecda4bef7
    CR-Id: ALPS03967548
    Feature: [Module]MET (Mediatek Embedded Technology)

--- Android.mk
+++ Android.mk
@@ -5,10 +5,19 @@ ifneq (,$(filter $(word 2,$(subst -, ,$(LINUX_KERNEL_VERSION))),$(subst /, ,$(LO
 MY_KERNEL_ROOT_DIR := $(PWD)
 MY_KERNEL_CONFIG_FILE := $(MY_KERNEL_ROOT_DIR)/kernel-4.4/arch/$(TARGET_ARCH)/configs/$(KERNEL_DEFCONFIG)
 MY_KERNEL_CONFIG_MODULES := $(shell grep ^CONFIG_MODULES=y $(MY_KERNEL_CONFIG_FILE))
-MY_TEST := CONFIG_MODULES=y
+
+MY_KERNEL_DEFAULT_CONFIG_FILE := $(MY_KERNEL_ROOT_DIR)/kernel-4.4/drivers/misc/mediatek/Kconfig.default
+MY_KERNEL_DEFAULT_CONFIG_MODULES := $(shell grep "select MODULES" $(MY_KERNEL_DEFAULT_CONFIG_FILE))
+
+CONFIG_MODULES_EXIST = n
+ifneq ($(MY_KERNEL_CONFIG_MODULES), "")
+	CONFIG_MODULES_EXIST := y
+else ifneq ($(MY_KERNEL_DEFAULT_CONFIG_MODULES), "")
+	CONFIG_MODULES_EXIST := y
+endif
 
 # we should not build ko for some project without define CONFIG_MODULES
-ifeq ($(MY_KERNEL_CONFIG_MODULES),$(MY_TEST))
+ifeq ($(CONFIG_MODULES_EXIST), y)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := met.ko
@@ -21,6 +30,10 @@ ifeq (user,$(TARGET_BUILD_VARIANT))
 endif
 
 include $(MTK_KERNEL_MODULE)
-
-endif # $(MY_KERNEL_CONFIG_MODULES) == $(MY_TEST)
+else
+$(warning Not building met.ko due to CONFIG_MODULES is not set)
+$(warning Please check following config files whether CONFIG_MODULES is set)
+$(warning 1. $(MY_KERNEL_CONFIG_FILE))
+$(warning 2. $(MY_KERNEL_DEFAULT_CONFIG_FILE))
+endif # $(CONFIG_MODULES_EXIST == y)
 endif # Kernel version matches current path
